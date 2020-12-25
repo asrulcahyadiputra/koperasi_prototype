0838f90eed7d8e69b4fa60caa91b77dfc732d9b1
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2020-12-26 05:41:45 +0700

    Update Database V 26-12-2020

--- db/koperasi_db.sql
+++ db/koperasi_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 25, 2020 at 03:24 PM
+-- Generation Time: Dec 25, 2020 at 10:41 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -34,6 +34,7 @@ CREATE TABLE `akun` (
 INSERT INTO `akun` (`kode_akun`, `nama_akun`, `saldo_normal`, `kode_sub_akun`) VALUES
 ('110001', 'Kas', 'd', '11'),
 ('110002', 'Piutang Usaha', 'd', '11'),
+('310001', 'Simpanan Wajib', 'k', '31'),
 ('410001', 'Pendapatan Jasa ', 'k', '41');
 
 -- --------------------------------------------------------
@@ -64,6 +65,35 @@ INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `alamat_anggota`, `no_hp`, 
 ('AGT-000003', 'Nur Indah Sari', 'Bandung Jawa Barat', '085258852369', 'indahsari@gmail.com', 1, '2020-12-24 20:47:39', '2020-12-24 21:07:54', NULL),
 ('AGT-000004', 'Djoko Handoko', 'Bandung Jawa Barat', '082546654789', 'djoko@gmail.com', 1, '2020-12-24 20:52:58', NULL, NULL),
 ('AGT-000005', 'Lilis', 'Bandung Jawa Barat', '081789123321', 'lilis123@gmail.com', 0, '2020-12-24 21:12:30', '2020-12-24 21:12:56', '2020-12-24 21:12:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jurnal_umum`
+--
+
+CREATE TABLE `jurnal_umum` (
+  `id_jurnal` bigint(20) NOT NULL,
+  `tanngal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `kode_akun` char(20) NOT NULL,
+  `id_transaksi` varchar(20) NOT NULL,
+  `nominal` double NOT NULL,
+  `posisi` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jurnal_umum`
+--
+
+INSERT INTO `jurnal_umum` (`id_jurnal`, `tanngal`, `kode_akun`, `id_transaksi`, `nominal`, `posisi`) VALUES
+(1, '2020-12-25 22:03:42', '110001', 'TRX-STR-000000001', 250000, 'd'),
+(2, '2020-12-25 22:03:42', '310001', 'TRX-STR-000000001', 250000, 'k'),
+(3, '2020-12-25 22:13:23', '110001', 'TRX-STR-000000002', 250000, 'd'),
+(4, '2020-12-25 22:13:23', '310001', 'TRX-STR-000000002', 250000, 'k'),
+(5, '2020-12-25 22:14:32', '110001', 'TRX-STR-000000003', 250000, 'd'),
+(6, '2020-12-25 22:14:32', '310001', 'TRX-STR-000000003', 250000, 'k'),
+(7, '2020-12-25 22:14:47', '110001', 'TRX-STR-000000004', 250000, 'd'),
+(8, '2020-12-25 22:14:47', '310001', 'TRX-STR-000000004', 250000, 'k');
 
 -- --------------------------------------------------------
 
@@ -148,6 +178,30 @@ INSERT INTO `sub_akun` (`kode_sub_akun`, `nama_sub_akun`, `kode_kepala_akun`) VA
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(20) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_anggota` varchar(20) DEFAULT NULL,
+  `total` double NOT NULL,
+  `trans_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `tanggal`, `id_anggota`, `total`, `trans_type`) VALUES
+('TRX-STR-000000001', '2020-12-25 22:03:42', 'AGT-000001', 250000, 'penyetoran_anggota'),
+('TRX-STR-000000002', '2020-12-25 22:13:23', 'AGT-000002', 250000, 'penyetoran_anggota'),
+('TRX-STR-000000003', '2020-12-25 22:14:32', 'AGT-000003', 250000, 'penyetoran_anggota'),
+('TRX-STR-000000004', '2020-12-25 22:14:47', 'AGT-000004', 250000, 'penyetoran_anggota');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -190,6 +244,13 @@ ALTER TABLE `anggota`
   ADD PRIMARY KEY (`id_anggota`);
 
 --
+-- Indexes for table `jurnal_umum`
+--
+ALTER TABLE `jurnal_umum`
+  ADD PRIMARY KEY (`id_jurnal`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
 -- Indexes for table `kepala_akun`
 --
 ALTER TABLE `kepala_akun`
@@ -209,11 +270,28 @@ ALTER TABLE `sub_akun`
   ADD KEY `kode_kepala_akun` (`kode_kepala_akun`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_anggota` (`id_anggota`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD KEY `id_pegawai` (`id_pegawai`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jurnal_umum`
+--
+ALTER TABLE `jurnal_umum`
+  MODIFY `id_jurnal` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
@@ -226,10 +304,22 @@ ALTER TABLE `akun`
   ADD CONSTRAINT `akun_ibfk_1` FOREIGN KEY (`kode_sub_akun`) REFERENCES `sub_akun` (`kode_sub_akun`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `jurnal_umum`
+--
+ALTER TABLE `jurnal_umum`
+  ADD CONSTRAINT `jurnal_umum_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `sub_akun`
 --
 ALTER TABLE `sub_akun`
   ADD CONSTRAINT `sub_akun_ibfk_1` FOREIGN KEY (`kode_kepala_akun`) REFERENCES `kepala_akun` (`kode_kepala_akun`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
