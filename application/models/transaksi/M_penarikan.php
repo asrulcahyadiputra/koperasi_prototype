58b584642960e6bd7ab8a5f917047e878ade4711
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2020-12-27 00:48:58 +0700

    Transaksi Penarikan"

---
+++ application/models/transaksi/M_penarikan.php
@@ -0,0 +1,153 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_penarikan extends CI_Model
+{
+	public function GetAnggotaAktif()
+	{
+		return $this->db->get_where('anggota', ['status' => 1])->result_array();
+	}
+
+	public function GetTransaksi()
+	{
+		$this->db->select('a.id_transaksi,a.tanggal,a.id_anggota,a.total,b.nama_anggota')
+			->from('transaksi as a')
+			->join('anggota as b', 'a.id_anggota=b.id_anggota')
+			->where('trans_type', 'penarikan_anggota');
+		return $this->db->get()->result_array();
+	}
+
+	private function id_transaksi()
+	{
+		$this->db->select('RIGHT(id_transaksi,9) as id_transaksi', FALSE);
+		$this->db->where('trans_type', 'penarikan_anggota');
+		$this->db->order_by('id_transaksi', 'DESC');
+		$this->db->limit(1);
+		$query = $this->db->get('transaksi');
+		if ($query->num_rows() <> 0) {
+			$data = $query->row();
+			$id = intval($data->id_transaksi) + 1;
+		} else {
+			$id = 1;
+		}
+		$code = str_pad($id, 9, "0", STR_PAD_LEFT);
+		$id_transaksi = "TRX-PNR-" . $code;
+		return $id_transaksi;
+	}
+	public function find_saldo($id)
+	{
+		$response = $this->db->query("SELECT 
+		a.id_anggota,
+		a.nama_anggota,
+		tb_saldo.setoran - tb_saldo.penarikan as saldo
+		FROM 
+		anggota as a 
+		LEFT OUTER JOIN (
+			SELECT id_anggota,sum(IF(trans_type='penyetoran_anggota',total,0)) as setoran, sum(IF(trans_type='penarikan_anggota',total,0)) as penarikan 
+			FROM transaksi 
+			GROUP BY id_anggota
+		)as tb_saldo
+		ON a.id_anggota=tb_saldo.id_anggota
+		WHERE a.id_anggota = '$id' ")->row_array();
+		return $response;
+	}
+	public function insert()
+	{
+		$id_transaksi 		= $this->id_transaksi();
+		$id_anggota 		= $this->input->post('id_anggota');
+		$total			= intval(preg_replace("/[^0-9]/", "", $this->input->post('total')));
+		$saldo              = $this->find_saldo($id_anggota);
+		if ($total > 0) {
+			if ($saldo['saldo'] >= $total) {
+				$data = [
+					'id_transaksi'			=> $id_transaksi,
+					'id_anggota'			=> $id_anggota,
+					'total'				=> $total,
+					'trans_type'			=> 'penarikan_anggota'
+				];
+				$gl = [
+					[
+						'kode_akun'		=> '310001',
+						'id_transaksi'		=> $id_transaksi,
+						'nominal'			=> $total,
+						'posisi'			=> 'd'
+					],
+					[
+						'kode_akun'		=> '110001',
+						'id_transaksi'		=> $id_transaksi,
+						'nominal'			=> $total,
+						'posisi'			=> 'k'
+					]
+				];
+				$this->db->trans_start();
+				$this->db->insert('transaksi', $data);
+				$this->db->insert_batch('jurnal_umum', $gl);
+				$this->db->trans_complete();
+
+				$response = [
+					'status'			=> 'OK',
+					'label'			=> 'success',
+					'msg'			=> 'Penarikan Simpanan Berhasil Ditambahkan !'
+				];
+			} else {
+				$response = [
+					'status'			=> 'BAD REQUEST',
+					'label'			=> 'error',
+					'msg'			=> 'Saldo TIdak Cukup !'
+				];
+			}
+		} else {
+			$response = [
+				'status'			=> 'BAD REQUEST',
+				'label'			=> 'error',
+				'msg'			=> 'Penarikan Simpanan Gagal Ditambahkan !'
+			];
+		}
+		return $response;
+	}
+
+	public function update()
+	{
+		$id_transaksi 		= $this->input->post('id_transaksi');
+		$id_anggota 		= $this->input->post('id_anggota');
+		$total			= intval(preg_replace("/[^0-9]/", "", $this->input->post('total')));
+
+		if ($total > 0) {
+			$data = [
+				'id_anggota'			=> $id_anggota,
+				'total'				=> $total,
+				'trans_type'			=> 'penyetoran_anggota'
+			];
+			$gl = [
+				[
+					'id_transaksi'		=> $id_transaksi,
+					'nominal'			=> $total
+				],
+				[
+					'id_transaksi'		=> $id_transaksi,
+					'nominal'			=> $total
+				]
+			];
+			$this->db->trans_start();
+			$this->db->update('transaksi', $data, ['id_transaksi' => $id_transaksi]);
+			$this->db->update_batch('jurnal_umum', $gl, 'id_transaksi');
+			$this->db->trans_complete();
+
+			$response = [
+				'status'			=> 'OK',
+				'label'			=> 'success',
+				'msg'			=> 'Penyetoran Simpanan Berhasil Diedit !'
+			];
+		} else {
+			$response = [
+				'status'			=> 'BAD REQUEST',
+				'label'			=> 'error',
+				'msg'			=> 'Penyetoran Simpanan Gagal Diedit !'
+			];
+		}
+		return $response;
+	}
+}
+
+/* End of file M_penarikan.php */
---
+++ application/views/transaksi/penarikan/data_penarikan.php
@@ -0,0 +1,143 @@
+<?php $this->load->view('_partials/header'); ?>
+<!-- ============================================================== -->
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
+	<!-- ============================================================== -->
+	<!-- Bread crumb and right sidebar toggle -->
+	<!-- ============================================================== -->
+	<div class="page-breadcrumb">
+		<div class="row">
+			<div class="col-7 align-self-center">
+				<h3 class="page-title text-truncate text-dark font-weight-medium mb-1"><?= $title ?></h3>
+				<div class="d-flex align-items-center">
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb m-0 p-0">
+							<li class="breadcrumb-item"><a href="<?= site_url('dashboard') ?>">Dashboard</a>
+							</li>
+							<li class="breadcrumb-item"><a href="<?= site_url('transaksi/penarikan') ?>"><?= $title ?></a>
+							</li>
+						</ol>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- ============================================================== -->
+	<!-- End Bread crumb and right sidebar toggle -->
+	<!-- ============================================================== -->
+
+	<!-- ============================================================== -->
+	<!-- Container fluid  -->
+	<!-- ============================================================== -->
+	<div class="container-fluid">
+		<!-- basic table -->
+		<div class="row">
+			<div class="col-12 mb-2">
+				<a href="" class="btn btn-primary" data-toggle="modal" data-target="#add-modal">
+					<i data-feather="plus" class="feather-icon"></i>
+					<span>Buat Penarikan</span>
+				</a>
+
+				<?php if ($this->session->flashdata('success')) : ?>
+					<div class="alert alert-success alert-dismissible fade show mt-3 col-md-12" role="alert">
+						<strong>Berhasil !</strong> <?= $this->session->flashdata('success') ?>
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+				<?php endif ?>
+				<?php if ($this->session->flashdata('error')) : ?>
+					<div class="alert alert-danger alert-dismissible fade show mt-3 col-md-12" role="alert">
+						<strong>Gagal !</strong> <?= $this->session->flashdata('error') ?>
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+				<?php endif ?>
+				<?php if ($this->session->flashdata('warning')) : ?>
+					<div class="alert alert-warning alert-dismissible fade show mt-3 col-md-12" role="alert">
+						<strong>Peringatan !</strong> <?= $this->session->flashdata('warning') ?>
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+				<?php endif ?>
+			</div>
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body">
+						<div class="table-responsive">
+							<table id="zero_config" class="table table-striped table-bordered no-wrap">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Tanggal</th>
+										<th>Anggota</th>
+										<th>Total Penarikan</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $no = 1;
+									foreach ($transaksi as $t) : ?>
+										<tr>
+											<td><?= $no++ ?></td>
+											<td>
+												<?= date('d-m-Y', strtotime($t['tanggal'])) ?>
+											</td>
+											<td>
+												<?= $t['id_anggota'] . ' ' . $t['nama_anggota'] ?>
+											</td>
+											<td>
+												<?= nominal($t['total']) ?>
+											</td>
+										</tr>
+									<?php endforeach ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- ============================================================== -->
+	<!-- End Container fluid  -->
+	<!-- ============================================================== -->
+
+	<!-- Primary Header Modal -->
+	<div id="add-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="primary-header-modalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header modal-colored-header bg-primary">
+					<h4 class="modal-title" id="primary-header-modalLabel">Buat <?= $title ?>
+					</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				</div>
+				<form action="<?= site_url('transaksi/penarikan/simpan') ?>" method="POST" class="needs-validation" novalidate>
+					<div class="modal-body">
+						<div class="form-group">
+							<label for="id_anggota">Anggota</label>
+							<select name="id_anggota" id="id_anggota" class="form-control">
+								<option value="">-pilih anggota-</option>
+								<?php foreach ($anggota as $ag) : ?>
+									<option value="<?= $ag['id_anggota'] ?>"><?= $ag['id_anggota'] . ' ' . $ag['nama_anggota'] ?></option>
+								<?php endforeach ?>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<label for="total">Jumlah Penarikan</label>
+							<input type="text" name="total" id="total" class="form-control" data-type="currency" required>
+						</div>
+						<h2>Total Saldo : <b id="saldo"></b> </h2>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-light" data-dismiss="modal">Batal</button>
+						<button type="submit" class="btn btn-primary">Simpan</button>
+					</div>
+				</form>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+	<?php $this->load->view('_partials/footer'); ?>
